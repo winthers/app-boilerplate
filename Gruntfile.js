401006c1b38fb04e7f885baf4748c576922659c3
martin <mbw@cadpeople.dk>
2013-12-13 14:26:29 +0100

    reformatted

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,34 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 
-        pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 
-        /**
-         * Configure Project Settings
-         * ==========================
-         */
-        settings: {
+		/**
+		 * Configure Project Settings
+		 * ==========================
+		 */
+		settings: {
 
-            production: false
-            , pageTitle:  "HTML5/SPA Project Template",
-        },
+			production: false,
+			pageTitle: "HTML5/SPA Project Template",
+		},
 
 		/**
 		 * CSS PRE PROCESSOR
 		 * =================
 		 * https://npmjs.org/package/grunt-contrib-compass*/
-		 
+
 		compass: {
 			dist: {
 				options: {
-					 sassDir:                   "./src/scss"
-					,cssDir:                   	"./public/assets/css"
-					,imagesDir:			       	"./public/assets/images/"
-					,generatedImagesDir: 		"./public/assets/images/sprites"
-					,generatedImagesPath: 		"./public/assets/images/sprites"
-					,httpGeneratedImagesPath: 	"../public/assets/images/sprites"
-          			,raw:                      	"preferred_syntax = :sass\n" // Use `raw` since it's not directly available
-                }
+					sassDir: "./src/scss",
+					cssDir: "./public/assets/css",
+					imagesDir: "./public/assets/images/",
+					generatedImagesDir: "./public/assets/images/sprites",
+					generatedImagesPath: "./public/assets/images/sprites",
+					httpGeneratedImagesPath: "../public/assets/images/sprites",
+					raw: "preferred_syntax = :sass\n" // Use `raw` since it's not directly available
+				}
 			}
 		},
 
@@ -42,11 +42,10 @@ module.exports = function(grunt) {
 		jst: {
 			compile: {
 				options: {
-				  prettify: true
+					prettify: true
 				},
 				files: {
-					"public/assets/js/templates.js": 
-					["src/js/app/apps/**/*.tpl"]
+					"public/assets/js/templates.js": ["src/js/app/apps/**/*.tpl"]
 				}
 			}
 		},
@@ -54,7 +53,7 @@ module.exports = function(grunt) {
 		/**
 		 * (WATCH) Detect file changes
 		 * ============================
-    	 * https://github.com/gruntjs/grunt-contrib-watch
+		 * https://github.com/gruntjs/grunt-contrib-watch
 		 */
 		watch: {
 
@@ -68,67 +67,68 @@ module.exports = function(grunt) {
 				tasks: ["jst"]
 			},
 
-			indexTemplate: { 
+			indexTemplate: {
 				// --  Changes to the index template will trigger a recompile - and a livereload
 				files: ["src/views/index.tpl"],
 				tasks: ["preprocess", "scriptincluder"]
 			},
 
-		    liveReloadOnCssChanges: {
-		        options: {livereload: true},
-		        files: [
-		        	"public/assets/css/main.css"
-		        ]
-		    },
-    		liveReloadOn_HTML_and_JS_Changes: {
-		        options: {livereload: true},
-		        files: [
-		        	"public/index.html"
-		        ]
-		    },
+			liveReloadOnCssChanges: {
+				options: {
+					livereload: true
+				},
+				files: [
+					"public/assets/css/main.css"
+				]
+			},
+			liveReloadOn_HTML_and_JS_Changes: {
+				options: {
+					livereload: true
+				},
+				files: [
+					"public/index.html"
+				]
+			},
 		},
 
 		/**
 		 * Combine Files
-	     * =============
-	     * https://github.com/gruntjs/grunt-contrib-concat
+		 * =============
+		 * https://github.com/gruntjs/grunt-contrib-concat
 		 */
 		concat: {
 
-		    options: {
-		        separator: '\n /***************************/ \n'
-		    },
+			options: {
+				separator: '\n /***************************/ \n'
+			},
 
-            /* Core dependicies */
-		    libs: {
-		        src: [
-	                 "src/js/lib/jquery.1.9.1.min.js"
-	              	,"src/js/lib/underscore.1.5.2.min.js"
-	              	,"src/js/lib/backbone.1.0.0.min.js"
-	              	,"src/js/lib/backbone.marionette.1.1.0.min.js"
-                ],
-                dest: 'public/assets/js/lib.js'
-		    },
+			/* Core dependicies */
+			libs: {
+				src: [
+					"src/js/lib/jquery.1.9.1.min.js", "src/js/lib/underscore.1.5.2.min.js", "src/js/lib/backbone.1.0.0.min.js", "src/js/lib/backbone.marionette.1.1.0.min.js"
+				],
+				dest: 'public/assets/js/lib.js'
+			},
 
 
-		    dependiciesConfiguration: {
-		    	src: ["src/js/app/config/**/*.js"],
-		    	dest: "public/assets/js/config.js"
-		    },
+			dependiciesConfiguration: {
+				src: ["src/js/app/config/**/*.js"],
+				dest: "public/assets/js/config.js"
+			},
 
-            /* Application files */
-            app: {
-                src: [
-                 	"src/js/app/app.js",
-                 	"src/js/app/base/**/*.js",
-                 	"src/js/app/utils/**/*.js",
-                 	"src/js/app/mixins/**/*.js",
-                 	"src/js/app/entities/**/*.js",
-                 	"src/js/app/components/**/*.js",
-                 	"src/js/app/apps/**/*.js"
-                ],
-                dest: "public/assets/js/app.js"
-            }
+			/* Application files */
+			app: {
+				src: [
+					"src/js/app/app.js",
+					"src/js/app/base/**/*.js",
+					"src/js/app/utils/**/*.js",
+					"src/js/app/mixins/**/*.js",
+					"src/js/app/entities/**/*.js",
+					"src/js/app/components/**/*.js",
+					"src/js/app/apps/**/*.js"
+				],
+				dest: "public/assets/js/app.js"
+			}
 		},
 
 		scriptincluder: {
@@ -151,49 +151,49 @@ module.exports = function(grunt) {
 
 		/**
 		 * Removes logging from files
-     	 * ==========================
+		 * ==========================
 		 * use my custom tweked version at:
 		 * https://github.com/winthers/grunt-remove-logging
-		*/
+		 */
 		removelogging: {
 			dist: {
 				src: "public/js/app.js",
 				dest: "public/js/app.js"
 			}
-		}, 
+		},
 
-	    /**
-         * Minify javascript
-         * =================
-         * https://github.com/gruntjs/grunt-contrib-uglify
-         */
+		/**
+		 * Minify javascript
+		 * =================
+		 * https://github.com/gruntjs/grunt-contrib-uglify
+		 */
 		uglify: {
-    		my_target: {
-    			 options: {
- 			       beautify: false
-      			},
-      			files: {
-        			'public/js/app.min.js': ['public/js/app.js']
-      			}
-    		}
-  		},
+			my_target: {
+				options: {
+					beautify: false
+				},
+				files: {
+					'public/js/app.min.js': ['public/js/app.js']
+				}
+			}
+		},
 
 
-  		/**
-  	 	 * Minify html
-  	 	 * ========================
-  	 	 * https://github.com/jney/grunt-htmlcompressor
-  	 	 */
+		/**
+		 * Minify html
+		 * ========================
+		 * https://github.com/jney/grunt-htmlcompressor
+		 */
 		htmlcompressor: {
-		    compile: {
-		      	files: {
-		        	'public/index-min.html': 'public/index.html'
-		      	},
-		      	options: {
-		        	type: 'html',
-		        	preserveServerScript: true
-		      	}
-		   	}
+			compile: {
+				files: {
+					'public/index-min.html': 'public/index.html'
+				},
+				options: {
+					type: 'html',
+					preserveServerScript: true
+				}
+			}
 		},
 
 		/**
@@ -202,85 +202,83 @@ module.exports = function(grunt) {
 		 * https://npmjs.org/package/grunt-pngmin
 		 */
 		pngmin: {
-		    compile: {
-		      	options: {
-			        concurrency: 8,             // specify how many exucutables get spawned in parallel
-			        colors: 128,                // reduce colors to 128
-			        ext: '.png',                // use .png as extension for the optimized files
-			        quality: '65-80',           // output quality should be between 65 and 80 like jpeg quality
-			        speed: 10                  // pngquant should be as fast as possible
-		      	},
-		      	files: [
-			        {
+			compile: {
+				options: {
+					concurrency: 8, // specify how many exucutables get spawned in parallel
+					colors: 128, // reduce colors to 128
+					ext: '.png', // use .png as extension for the optimized files
+					quality: '65-80', // output quality should be between 65 and 80 like jpeg quality
+					speed: 10 // pngquant should be as fast as possible
+				},
+				files: [{
 
-			          	expand: true, // required option
-			          	src: ['**/*.png'],
-			          	cwd: './public/assets/images/sprites/', // required option
-			          	dest: './public/assets/images/sprites/'
-			        }
-		      	]
-		    }
+					expand: true, // required option
+					src: ['**/*.png'],
+					cwd: './public/assets/images/sprites/', // required option
+					dest: './public/assets/images/sprites/'
+				}]
+			}
 		},
 
-  		/**
-  		 * DELETE FILES
-  		 * ============
-         * https://github.com/gruntjs/grunt-contrib-clean
-         *
-  		 * WARNING: This will delete files, so be carefule what u point it at
-  		 * This can be configured like this: http://gruntjs.com/configuring-tasks
-  		 */
-  		clean: {
-  			tempFiles: {
-  				src: [
-  					".sass-cache"
-  				]
-  			}
-  		},
+		/**
+		 * DELETE FILES
+		 * ============
+		 * https://github.com/gruntjs/grunt-contrib-clean
+		 *
+		 * WARNING: This will delete files, so be carefule what u point it at
+		 * This can be configured like this: http://gruntjs.com/configuring-tasks
+		 */
+		clean: {
+			tempFiles: {
+				src: [
+					".sass-cache"
+				]
+			}
+		},
 
 
-  		// maybe replace with https://github.com/yeoman/grunt-usemin
-        /**
-         * PROD / DEV
-         * ==========
-         * https://github.com/jsoverson/grunt-preprocess
-         *
-         * Creates diffrent index.html based on the production:boolean value.
-         */
-        preprocess: {
-            options: {
-                context: {
-                    DEBUG: true,
-                    production: "<%= settings.production %>",
-                    pageTitle: 	"<%= settings.pageTitle %>"
-                }
-            },
-            html: {
-                src: 'src/views/index.tpl',
-                dest: 'public/index.html'
-            }
+		// maybe replace with https://github.com/yeoman/grunt-usemin
+		/**
+		 * PROD / DEV
+		 * ==========
+		 * https://github.com/jsoverson/grunt-preprocess
+		 *
+		 * Creates diffrent index.html based on the production:boolean value.
+		 */
+		preprocess: {
+			options: {
+				context: {
+					DEBUG: true,
+					production: "<%= settings.production %>",
+					pageTitle: "<%= settings.pageTitle %>"
+				}
+			},
+			html: {
+				src: 'src/views/index.tpl',
+				dest: 'public/index.html'
+			}
 
-        },
+		},
 
 		/*
 			https://github.com/kmiyashiro/grunt-mocha
 		*/
-        mocha: {
-        	all: {
-            	src: ['test/**/*.html'],
-                options: {
-                    run: true
-                }
-            }
-        
-        },
+		mocha: {
+			all: {
+				src: ['test/**/*.html'],
+				options: {
+					run: true
+				}
+			}
+
+		},
 
 	});
 
 	// ----------------------------------------------
 	// Register tasks.
 	// ----------------------------------------------
-	
+
 	// Development
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-jst');
@@ -295,11 +293,11 @@ module.exports = function(grunt) {
 	// HTML Minify 
 	grunt.loadNpmTasks('grunt-htmlcompressor');
 	grunt.loadNpmTasks('grunt-pngmin');
-	
+
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
-    
-    grunt.loadNpmTasks('grunt-preprocess');
+
+	grunt.loadNpmTasks('grunt-preprocess');
 
 	grunt.loadNpmTasks('grunt-mocha');
 	grunt.loadNpmTasks('grunt-scriptincluder');
@@ -307,13 +305,12 @@ module.exports = function(grunt) {
 	// ----------------------------------------------
 	// Default task.
 	// ----------------------------------------------
-	grunt.registerTask('default', ["watch"]); 
-
+	grunt.registerTask('default', ["watch"]);
 
 
 
 	grunt.registerTask("build:prod", ["compass", "jst", "concat", "uglify", "clean", "preprocess"]);
-	grunt.registerTask("build:dev",  ["compass", "jst", "concat", "preprocess", "scriptincluder"]);
+	grunt.registerTask("build:dev", ["compass", "jst", "concat", "preprocess", "scriptincluder"]);
 
 
 
