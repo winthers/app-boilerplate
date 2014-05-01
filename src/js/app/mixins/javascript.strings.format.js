2de02686c8cf44d3fa62e8a9a5d588fa4ee5ece0
martin <mbw@cadpeople.dk>
2014-05-01 13:13:28 +0200

    added the interface sprite folder and some scss stuff and the format javascript mixin

---
+++ src/js/app/mixins/javascript.strings.format.js
@@ -0,0 +1,12 @@
+
+// "{0} {1}".format("hello", "world") // hello world
+String.prototype.format = function()
+{
+   var content = this;
+   for (var i=0; i < arguments.length; i++)
+   {
+        var replacement = '{' + i + '}';
+        content = content.replace(replacement, arguments[i]);  
+   }
+   return content;
+};
